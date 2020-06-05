5d4bee783723d15dc00bbed8698f7e2b6a41b414
topTalent1212 <topone1212talent@gmail.com>
2020-06-05 13:04:01 +0200

    init

---
+++ Per/DataContext.cs
@@ -0,0 +1,26 @@
+using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Per
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+            .HasData(
+                new Value { Id = 1, Name = "Value 111" },
+                new Value { Id = 2, Name = "Value 222" },
+                new Value { Id = 3, Name = "Value 333" },
+                new Value { Id = 4, Name = "Value 444" }
+            );
+        }
+    }
+}
