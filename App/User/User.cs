39fede4b886bae45b5bdb2213d1725d6967d82d6
topTalent1212 <topone1212talent@gmail.com>
2020-06-18 15:01:10 +0200

    Auth: login, registration, jwt tokens

---
+++ App/User/User.cs
@@ -0,0 +1,10 @@
+namespace App.User
+{
+    public class User
+    {
+        public string DisplayName { get; set; }
+        public string Token { get; set; }
+        public string Username { get; set; }
+        public string Image { get; set; }
+    }
+}
